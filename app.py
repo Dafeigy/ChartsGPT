f92019e96a69601757415ccb085aa054942869e9
Dafeigy <1070642765@qq.com>
2023-05-10 17:40:22 +0800

    <Update>:Data structure done.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template, request
+import json
 import utils
 
 app = Flask(__name__)
@@ -8,9 +9,58 @@ def homepage():
     return render_template("main.html")
     
 @app.route("/json",methods=['GET', 'POST'])
-def json():
-    message = utils.get_requests()
-    return utils.get_response(message)
+def response():
+    # message = utils.get_requests()
+    # return utils.get_response(message)
+    return json.dumps(
+{
+    "title": {
+        "left": "center"
+    },
+    "tooltip": {},
+    "legend": {
+        "orient": "vertical",
+        "left": "left"
+    },
+    "xAxis": {
+        "type": "category",
+        "data": [
+            "Mon",
+            "Tue",
+            "Wed",
+            "Thu",
+            "Fri",
+            "Sat",
+            "Sun"
+        ]
+    },
+    "yAxis": {
+        "type": "value"
+    },
+    "series": [
+        {
+            "type": "line",
+            "data": [
+                150,
+                230,
+                224,
+                218,
+                135,
+                147,
+                260
+            ]
+        }
+    ]
+}
+    )
     
+
+@app.route("/ajax",methods=['POST','GET'])
+def ajax():
+    message = request.json
+    return message
+
+
+
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000,debug=True)
